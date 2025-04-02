44359eafab78412ef83881f5371e783cf17175cc
samihasamin <samihasamin@hotmail.com>
2025-04-01 21:55:09 -0400

    Add responsive style for width 768px

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -9,20 +9,19 @@ function HeroSection() {
       <img src={ProfilePic} alt="Profile Image Hero" className="hero__pic" />
       <h1 className="hero__headline">
         <span>Hi! I’m Samiha Samin</span>A frontend developer based in Toronto.
-        <p className="hero__tagline">
-          Recent Bootcamp graduate skilled in JavaScript, React.js, CSS/SCSS,
-          Node.js, Express.js and MySQL. Winner of Microsoft's Industry
-          Hackathon.
-        </p>
-        <div className="hero__action">
-          <div className="hero__action-connect">
-            <AnchorLink className="anchor-link" offset={50} href="#Contact">
-              <p onClick={() => setMenu("Contact")}>Let's Connect</p>
-            </AnchorLink>
-          </div>
-          <div className="hero__action-resume">My Resume</div>
-        </div>
       </h1>
+      <p className="hero__tagline">
+        Recent Bootcamp graduate skilled in JavaScript, React.js, CSS/SCSS,
+        Node.js, Express.js and MySQL. Winner of Microsoft's Industry Hackathon.
+      </p>
+      <div className="hero__action">
+        <div className="hero__action-connect">
+          <AnchorLink className="anchor-link" offset={50} href="#Contact">
+            <p onClick={() => setMenu("Contact")}>Let's Connect</p>
+          </AnchorLink>
+        </div>
+        <div className="hero__action-resume">My Resume</div>
+      </div>
     </div>
   );
 }
